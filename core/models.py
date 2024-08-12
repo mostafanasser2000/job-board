de8c8207deb8f6367017dd37cdd3028ba1f15a5d
mostafanasser2000 <mustafanasser503@gmail.com>
2024-08-12 22:30:32 +0300

    add core models for application

--- core/models.py
+++ core/models.py
@@ -2,9 +2,40 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 from django.core.validators import MinValueValidator, URLValidator
+from django.utils import timezone
 
 
-class Company(models.Model):
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Industry(BaseModel):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=300, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug != slugify(self.name):
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Location(BaseModel):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+
+class Company(BaseModel):
     COMPANY_SIZE_CHOICES = [
         (1, "1-10"),
         (2, "11-50"),
@@ -17,7 +48,6 @@ class Company(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(unique=True, blank=True, db_index=True)
     company_site = models.URLField(blank=True, null=True, validators=[URLValidator()])
-    location = models.CharField(max_length=200)
     about = models.TextField(null=True, blank=True)
     company_size = models.PositiveIntegerField(
         null=True,
@@ -26,8 +56,10 @@ class Company(models.Model):
         validators=[MinValueValidator(1)],
     )
     linkedin_page = models.URLField(blank=True, null=True, validators=[URLValidator()])
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    industries = models.ManyToManyField(Industry, related_name="companies")
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, null=True, related_name="companies"
+    )
 
     class Meta:
         ordering = ["-created"]
@@ -35,7 +67,7 @@ class Company(models.Model):
         verbose_name_plural = "Companies"
 
     def save(self, *args, **kwargs):
-        if not self.slug:
+        if not self.slug or self.slug != slugify(self.name):
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
@@ -45,8 +77,14 @@ class Company(models.Model):
     def get_absolute_url(self):
         return reverse("company_detail", kwargs={"pk": self.pk})
 
+    def total_employees(self):
+        return self.COMPANY_SIZE_CHOICES[self.company_size][1]
 
-class Skill(models.Model):
+    def get_open_positions(self):
+        pass
+
+
+class Skill(BaseModel):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, blank=True, unique=True, db_index=True)
 
@@ -56,7 +94,7 @@ class Skill(models.Model):
         verbose_name_plural = "Skills"
 
     def save(self, *args, **kwargs):
-        if not self.slug:
+        if not self.slug or self.slug != slugify(self.name):
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
@@ -66,34 +104,52 @@ class Skill(models.Model):
     def get_related_jobs(self):
         return Job.objects.filter(skills_required=self)
 
+    @property
+    def related_jobs_count(self):
+        return self.jobs.count()
 
-class Job(models.Model):
+
+# class JobManager(models.Manager):
+#     def active(self):
+#         return self.filter(status="published")
+
+#     def by_skill(self, skill):
+#         return self.filter(skills_required=skill)
+
+
+class Job(BaseModel):
+    # STATUS = [
+    #     ("draft", "Draft"),
+    #     ("published", "Published"),
+    #     ("closed", "Closed"),
+    # ]
     WORKPLACE_TYPES = [
-        ("Remote", "Remote"),
-        ("Hybrid", "Hybrid"),
-        ("On-site", "On-site"),
+        ("remote", "Remote"),
+        ("hybrid", "Hybrid"),
+        ("on-site", "On-site"),
     ]
 
     WORK_TYPES = [
-        ("Full Time", "Full Time"),
-        ("Part Time", "Part Time"),
-        ("Internship", "Internship"),
-        ("Contract", "Contract"),
+        ("full_time", "Full Time"),
+        ("part_time", "Part Time"),
+        ("internship", "Internship"),
+        ("contract", "Contract"),
     ]
     CURRENCY_CHOICES = [
         ("USD", "US Dollar"),
         ("EUR", "Euro"),
         ("GBP", "British Pound"),
-        # Add more as needed
     ]
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=250, blank=True)
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
     company = models.ForeignKey(Company, related_name="jobs", on_delete=models.CASCADE)
     description = models.TextField()
     skills_required = models.ManyToManyField(Skill, related_name="jobs")
     workplace_type = models.CharField(max_length=20, choices=WORKPLACE_TYPES)
     work_type = models.CharField(max_length=20, choices=WORK_TYPES)
-    location = models.CharField(max_length=200)
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, null=True, related_name="jobs"
+    )
     salary_lowest = models.PositiveIntegerField(
         blank=True, null=True, validators=[MinValueValidator(0)]
     )
@@ -103,15 +159,16 @@ class Job(models.Model):
     salary_currency = models.CharField(
         max_length=3, choices=CURRENCY_CHOICES, default="USD"
     )
-    apply_link = models.URLField(
-        blank=True, null=True, validators=[MinValueValidator(0)]
-    )
-    created = models.DateTimeField(auto_created=True)
-    updated = models.DateTimeField(auto_now=True)
+    apply_link = models.URLField(blank=True, null=True, validators=[URLValidator()])
+
+    # objects = JobQuerySet.as_manager()
 
     class Meta:
         ordering = ["-created"]
-        indexes = [models.Index(fields=["title", "company", "location"])]
+        indexes = [
+            models.Index(fields=["title", "company", "location"]),
+            models.Index(fields=["-created"]),
+        ]
 
     def clean(self):
         if self.salary_highest and self.salary_lowest:
@@ -122,7 +179,7 @@ class Job(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(f"{self.title}-{self.company.name}-{self.location}")
+            self.slug = slugify(f"{self.title}-{self.company.name}-{self.id}")
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -130,3 +187,26 @@ class Job(models.Model):
 
     def get_absolute_url(self):
         return reverse("job_detail", kwargs={"pk": self.pk})
+
+    @property
+    def salary_range(self):
+        if self.salary_lowest and self.salary_highest:
+            return (
+                f"{self.salary_lowest} - {self.salary_highest} {self.salary_currency}"
+            )
+        elif self.salary_lowest:
+            return f"From {self.salary_lowest} {self.salary_currency}"
+        elif self.salary_highest:
+            return f"Up to {self.salary_highest} {self.salary_currency}"
+        else:
+            return "Not specified"
+
+    @classmethod
+    def get_recent_jobs(cls, days=7):
+        # return cls.objects.active().filter(
+        #     created__gte=timezone.now() - timezone.timedelta(days=days)
+        # )
+        pass
+
+    def apply_to_job(self, user):
+        pass
