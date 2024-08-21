9c549851ec5a70ef814450b10b9e87dd15206240
mostafanasser2000 <mustafanasser503@gmail.com>
2024-08-21 18:24:45 +0300

    register core models in the admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Job, Industry, Company, Skill
 
 # Register your models here.
+admin.site.register(Job)
+admin.site.register(Industry)
+admin.site.register(Company)
+admin.site.register(Skill)
