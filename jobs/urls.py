0bf241f3d9395442f0d75831313775270e6b1385
mostafanasser2000 <mustafanasser503@gmail.com>
2024-08-26 23:47:28 +0300

    update the project urls
    - add urls for accounts application
    - remove the urls of core application

--- jobs/urls.py
+++ jobs/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("core.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
